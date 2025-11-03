5590f938d085fdd0ff8e6a10b6002e30ef12562b
ashutoshdinde10 <ashutosh.dinde@coditas.com>
2025-11-03 11:23:51 +0530

    This is the test commit 12435 on the test

--- src/main/java/com/example/Criteria_Query_PMO_Demo/service/ProjectService.java
+++ src/main/java/com/example/Criteria_Query_PMO_Demo/service/ProjectService.java
@@ -23,7 +23,7 @@ public class ProjectService {
         this.projectRepository = projectRepository;
         this.entityManager = entityManager;
     }
-/// test 1234
+/// test 12345
     public String addProject(Project project){
 
         projectRepository.save(project);
